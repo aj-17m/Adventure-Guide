821b50a49e3b8fd3991a1d4788929f30e90444e0
Ajay Meena <22bcs018@iiitdmj.ac.in>
2025-02-03 15:34:13 +0530

    Setup

---
+++ src/data.js
@@ -0,0 +1,68 @@
+const data = [
+	{
+		id: 1,
+		name: "Agra",
+		info: "Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.",
+		image: "https://cdn.thecodehelp.in/Agra.jpeg",
+		price: "35,758",
+	},
+	{
+		id: 2,
+		name: "Jaipur",
+		info: `Jaipur is the capital city of the Indian state of Rajasthan, known for its stunning pink-hued buildings and palaces. The city is also known as the "Pink City" due to the color of many of its buildings. Jaipur is famous for its intricate textiles, colorful handicrafts, and delicious cuisine.`,
+		image: "https://cdn.thecodehelp.in/Jaipur.jpeg",
+		price: "82,560",
+	},
+	{
+		id: 3,
+		name: "Goa",
+		info: " Goa is a small state on India's western coast, known for its stunning beaches, vibrant nightlife, and Portuguese-influenced architecture. The state is also known for its spicy seafood dishes and unique cultural traditions.",
+		image: "https://cdn.thecodehelp.in/Goa.jpeg",
+		price: "29,695",
+	},
+	{
+		id: 4,
+		name: "Varanasi",
+		info: "Varanasi is a holy city in northern India, situated on the banks of the sacred river Ganges. The city is a major pilgrimage site for Hindus and is famous for its ancient temples, winding alleyways, and colorful festivals.",
+		image: "https://cdn.thecodehelp.in/Varanasi.jpeg",
+		price: "31,095",
+	},
+	{
+		id: 5,
+		name: "Darjeeling",
+		info: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest city. In this land of surprises — so trendy and hip, yet steeped in history — there's so much to discover. Join us for the Best of Poland in 10 Days!",
+		image: "https://cdn.thecodehelp.in/Darjeeling.jpeg",
+		price: "78,595",
+	},
+	{
+		id: 6,
+		name: "Jaisalmer",
+		info: "Jaisalmer is a city in the Indian state of Rajasthan, known for its stunning sandstone architecture and its location in the heart of the Thar Desert. The city is home to several impressive palaces and fortresses, as well as unique cultural traditions and handicrafts.",
+		image: "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+		price: "68,595",
+	},
+	{
+		id: 7,
+		name: "Kochi",
+		info: "Kochi is a port city in the Indian state of Kerala, known for its historic connections to trade and commerce. The city is home to several stunning churches, synagogues, and mosques, as well as a rich culinary tradition that blends Indian, Portuguese, and other influences.",
+		image: "https://cdn.thecodehelp.in/Kochi.jpeg",
+		price: "68,595",
+	},
+	// New Locations
+	{
+		id: 8,
+		name: "Leh",
+		info: "Leh is a town in the northern Indian region of Ladakh, known for its stunning landscapes, including snow-capped mountains, Buddhist monasteries, and crystal-clear lakes. It is a popular destination for trekkers, adventure seekers, and those looking to experience a unique high-altitude culture.",
+		image: "https://imgcld.yatra.com/ytimages/image/upload/v1462517217/Leh-Leh_Palace2.jpg",
+		price: "99,000",
+	},
+	{
+		id: 9,
+		name: "Udaipur",
+		info: "Udaipur, known as the 'City of Lakes,' is famous for its breathtaking palaces, temples, and serene lakes. The city is a romantic destination, with the iconic Lake Palace and City Palace offering stunning views and a rich history dating back to the Mewar dynasty.",
+		image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.SCxnUKdlhvG4SwM_9gzwEQHaFS%26pid%3DApi&f=1&ipt=b553a9e218f972e9c1f73706c988e95ca311e4c79a6bb14ede6c8a319411e66e&ipo=images",
+		price: "72,450",
+	},
+];
+
+export default data;
